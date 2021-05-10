068c818f0c3fbccb2267cba3f42fbf422010d9e5
Siddharth Udeniya <thundercut@Siddharths-MacBook-Pro.local>
2021-05-10 21:28:53 +0530

    Added routing in Node.js

---
+++ routing/app.js
@@ -0,0 +1,13 @@
+let http = require('http');
+http.createServer(function (req, res) {
+ res.setHeader('Content-Type', 'application/json');
+ let url = req.url;
+ if(url ==='/a'){
+    res.write(JSON.stringify({'message':'You have hit /a'}))
+    res.end()
+ }else{
+    res.write(JSON.stringify({'message':'You have hit something other than /a'}))
+    res.end()
+ }}).listen(8080, function(){
+ console.log("server start at port 8080"); //the server object listens on port 3000
+});
